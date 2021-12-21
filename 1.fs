e8972b1ec4bf8f4b0aab389ddd86973fe209f30d
Daniel Sklenitzka <daniel.sklenitzka@softaware.at>
2021-12-21 17:22:26 +0100

    1

---
+++ 1.fs
@@ -0,0 +1,22 @@
+﻿module AOC1
+open System
+open System.IO
+
+let handle (prev, count) curr =
+    if curr > prev
+    then (curr, count + 1)
+    else (curr, count)
+
+let countIncrements lines =
+    let (_, count) = 
+        lines |>
+        List.ofSeq |>
+        List.map Int32.Parse |>
+        List.fold handle (Int32.MaxValue, 0)
+    count
+
+let count =
+    File.ReadLines("1.txt") |>
+    countIncrements
+   
+printfn "%i" count
